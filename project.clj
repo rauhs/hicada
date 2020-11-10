83076f46063749f5135a8c602be28c449aeed3aa
Andre R <andre.rauh@gmail.com>
2020-11-10 08:40:48 +0100

    Release new 0.19

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hicada "0.1.8"
+(defproject hicada "0.1.9"
   :description "A hiccup compiler for clojurescript"
   :url "http://www.github.com/rauhs/hicada"
   :license {:name "Eclipse Public License"
